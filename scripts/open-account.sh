2039b7fe6b57ea3c6c7bf2c52a3296dbdca9d319
Roland Tritsch <roland@tritsch.org>
2019-08-27 19:34:21 +0100

    Make the manual test work.

--- scripts/open-account.sh
+++ scripts/open-account.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-curl -X POST -H 'application/json' '{"account": {"initial_balance": 1000}}' http://localhost:4000/api/accounts
+curl -X POST -H 'Content-Type: application/json' -d '{"account": {"initial_balance": 1000}}' http://localhost:4000/api/accounts
